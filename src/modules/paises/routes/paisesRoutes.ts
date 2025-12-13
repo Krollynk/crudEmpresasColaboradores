f427c1c9a39ca9d079e27c000cb2279760bf6113
Abner <87242234+Krollynk@users.noreply.github.com>
2025-12-13 15:25:59 -0600

    Update paisesRoutes.ts

--- src/modules/paises/routes/paisesRoutes.ts
+++ src/modules/paises/routes/paisesRoutes.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import paisesControllers from "../controllers/paisesControllers";
+import {paisesValidation} from "../../../middlewares/paisesValidation";
 
 const router = express();
 
 router.get('/', paisesControllers.getAllPaises);
-router.post('/', paisesControllers.createPais);
+router.post('/', paisesValidation, paisesControllers.createPais);
 router.route('/:pdcPaiId')
     .get(paisesControllers.getOnePais)
-    .patch(paisesControllers.updatePais)
+    .patch(paisesValidation, paisesControllers.updatePais)
     .delete(paisesControllers.deletePais);
 
 export default router;
