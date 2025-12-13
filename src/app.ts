bec54c6053647694841e7719e302b53172367471
Abner <87242234+Krollynk@users.noreply.github.com>
2025-12-13 15:09:38 -0600

    a

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,19 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import {Request, Response} from "express";
+import paisesRoutes from "./modules/paises/routes/paisesRoutes";
+import {errorHandler} from "./middlewares/errorHandler";
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors);
-app.use(morgan);
 app.get('/', (req: Request, res: Response)=> {
     console.log("Página de inicio");
     res.send("Página de inicio");
 });
-
+app.use('/paises', paisesRoutes);
+app.use(errorHandler);
 
 export default app;
